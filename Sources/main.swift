f7e077e14d91370ae85ef0dba9c7812ad56ed9a6
Satoshi Watanabe <sassy.watanabe@gmail.com>
2016-12-30 09:57:40 +0900

    til

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,18 @@
 
-var array = ["Hello,", "world!"]
+var a: String? = "テスト" //Optional
+print(a!)
+
+if var s = a {
+  print(s)
+} else {
+  print("s is nil")
+}
+
+let n: Int = 30
+print("Int is \(n)")
+
+var array: [String] = ["Hello,", "world!"]
+array.append("swift")
 for elem in array {
   print(elem)
 }
